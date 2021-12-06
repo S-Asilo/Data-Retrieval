a431b4aaeecc24a6bf685af2febf8a5602fc5e23
Asimakis Argyropoulos <makis31179@hotmail.com>
2021-12-06 16:01:26 +0200

    Almost Done with Part 5 and 6

--- Askisi2/Part5.py
+++ Askisi2/Part5.py
@@ -29,10 +29,42 @@ def cosine_sim(vec1, vec2):
 
 
 # Needs vectorization
-tokens = text4[:50]
+tokens = [text4[:50], text7[:50]]
 
-eng_stopwords = nltk.corpus.stopwords.words('english')
-tokens = [x for x in tokens if x not in eng_stopwords]
+# document_vector = []
+# doc_length = len(tokens)
 
+# eng_stopwords = nltk.corpus.stopwords.words('english')
+# tokens = [x for x in tokens if x not in eng_stopwords]
+# bag_of_words = Counter(tokens)
 
-bag_of_words = Counter(tokens)
+# for key, value in bag_of_words.most_common():   
+#     document_vector.append(value / doc_length)
+    
+all_doc_tokens = sum(tokens, [])
+
+lexicon = sorted(set(all_doc_tokens))    
+# print(len(lexicon))
+
+from collections import OrderedDict
+
+zero_vector = OrderedDict((token, 0) for token in lexicon)
+
+# print(zero_vector) 
+
+import copy
+
+doc_vectors = []
+for doc in tokens:
+    vec = copy.copy(zero_vector)
+    token_counts = Counter(doc)
+    
+    for key, value in token_counts.items():
+        vec[key] = value / len(lexicon)
+    
+    doc_vectors.append(vec)
+    
+    
+# print(len(doc_vectors))
+
+# print(doc_vectors[0])
